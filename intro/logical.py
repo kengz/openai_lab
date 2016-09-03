702450ed5c38dbce8f6210c2eee1e9ed73ac7464
kengz <kengzwl@gmail.com>
2016-09-03 15:48:45 -0400

    finish combo merge graph for XOR

--- intro/logical.py
+++ intro/logical.py
@@ -9,7 +9,7 @@ def train_model(g, X, Y, model_name='sample_model.tflearn'):
     m.save(model_name)
     # m.load(model_name)
 
-    print('Testing NOT operator')
+    print('Testing operator')
     print('Prediction', m.predict(X))
     print('Target Result', Y)
 
@@ -45,7 +45,72 @@ def run_OR():
     train_model(g, X, Y)
 
 
+# Logical AND operator
+def run_AND():
+    X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
+    Y = [[0.], [0.], [0.], [1.]]
+
+    g = tflearn.input_data(shape=[None, 2])
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 1, activation='sigmoid')
+    g = tflearn.regression(
+        g, optimizer='sgd', learning_rate=2., loss='mean_square')
+
+    train_model(g, X, Y)
+
+
+# Logical XOR operator
+def run_XOR():
+    X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
+    Y = [[0.], [1.], [1.], [0.]]
+
+    g = tflearn.input_data(shape=[None, 2])
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 1, activation='sigmoid')
+    g = tflearn.regression(
+        g, optimizer='sgd', learning_rate=2., loss='mean_square')
+
+    train_model(g, X, Y)
+
+
+# using graph combo with multiple optimizers
+# a bit of a cheat, but ok
+# Logical XOR operator
+def run_combo_XOR():
+    X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
+    Y_nand = [[1.], [1.], [1.], [0.]]
+    Y_or = [[0.], [1.], [1.], [1.]]
+
+    g = tflearn.input_data(shape=[None, 2])
+
+    # Nand graph
+    g_nand = tflearn.fully_connected(g, 32, activation='linear')
+    g_nand = tflearn.fully_connected(g_nand, 32, activation='linear')
+    g_nand = tflearn.fully_connected(g_nand, 1, activation='sigmoid')
+    g_nand = tflearn.regression(
+        g_nand, optimizer='sgd', learning_rate=2., loss='binary_crossentropy')
+
+    # Nand graph
+    g_or = tflearn.fully_connected(g, 32, activation='linear')
+    g_or = tflearn.fully_connected(g_or, 32, activation='linear')
+    g_or = tflearn.fully_connected(g_or, 1, activation='sigmoid')
+    g_or = tflearn.regression(
+        g_or, optimizer='sgd', learning_rate=2., loss='binary_crossentropy')
+
+    g_xor = tflearn.merge([g_nand, g_or], mode='elemwise_mul')
+
+    train_model(g_xor, X, [Y_nand, Y_or])
+    sess = tf.Session()  # separate from DNN session
+    print(
+        sess.run(tflearn.merge([Y_nand, Y_or], mode='elemwise_mul')))
+
+
 with tf.Graph().as_default():
     # visualization: tensorboard --logdir='/tmp/tflearn_logs'
     # run_NOT()
-    run_OR()
+    # run_OR()
+    # run_AND()
+    # run_XOR()
+    run_combo_XOR()
