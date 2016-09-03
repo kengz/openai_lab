673e1a70d79d6ed2defa80edebe553a6a2d06a2e
kengz <kengzwl@gmail.com>
2016-09-03 13:48:19 -0400

    refactor training method, add OR example

--- intro/logical.py
+++ intro/logical.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 import tflearn
 
 
+# train a model, given a graph g, data X, Y
+def train_model(g, X, Y, model_name='sample_model.tflearn'):
+    m = tflearn.DNN(g)
+    m.fit(X, Y, n_epoch=1000, snapshot_epoch=False)
+    m.save(model_name)
+    # m.load(model_name)
+
+    print('Testing NOT operator')
+    print('Prediction', m.predict(X))
+    print('Target Result', Y)
+
+
 # Logical NOT operator
 def run_NOT():
     X = [[0.], [1.]]
@@ -15,15 +27,7 @@ def run_NOT():
     g = tflearn.regression(
         g, optimizer='sgd', learning_rate=2., loss='mean_square')
 
-    # Train
-    m = tflearn.DNN(g)
-    m.fit(X, Y, n_epoch=1000, snapshot_epoch=False)
-    m.save('logical.tflearn')
-    # m.load('logical.tflearn')
-
-    print('Testing NOT operator')
-    print('Prediction', m.predict(X))
-    print('Target Result', Y)
+    train_model(g, X, Y)
 
 
 # Logical OR operator
@@ -31,7 +35,17 @@ def run_OR():
     X = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
     Y = [[0.], [1.], [1.], [1.]]
 
+    g = tflearn.input_data(shape=[None, 2])
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 128, activation='linear')
+    g = tflearn.fully_connected(g, 1, activation='sigmoid')
+    g = tflearn.regression(
+        g, optimizer='sgd', learning_rate=2., loss='mean_square')
+
+    train_model(g, X, Y)
+
 
 with tf.Graph().as_default():
     # visualization: tensorboard --logdir='/tmp/tflearn_logs'
-    run_NOT()
+    # run_NOT()
+    run_OR()
