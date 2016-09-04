43f5f22f29c555de227a269d59b53c4a0dbb3f98
kengz <kengzwl@gmail.com>
2016-09-04 11:39:08 -0400

    add better trainer usage in cnn eg, allows model saving and less manual coding

--- intro/extending_tensorflow_layers_cnn.py
+++ intro/extending_tensorflow_layers_cnn.py
@@ -4,6 +4,7 @@ import tflearn
 
 import tflearn.datasets.mnist as mnist
 mnist_data = mnist.read_data_sets(one_hot=True)
+trainX, trainY, testX, testY = mnist.load_data(one_hot=True)
 
 
 # convnet rule of thumb:
@@ -49,27 +50,46 @@ def run():
     net = tflearn.dropout(net, 0.8)
     net = tflearn.fully_connected(net, 10, activation='softmax')
 
-    # Defining other ops using Tensorflow
+    # --------------------------------------
+    # really manual tf way
+    # # Defining other ops using Tensorflow
+    # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(net, Y))
+    # optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    # optimizer_minop = optimizer.minimize(loss)
+
+    # # start
+    # init = tf.initialize_all_variables()
+
+    # with tf.Session() as sess:
+    #     sess.run(init)
+    #     batch_size = 128
+    #     for epoch in range(2):
+    #         avg_cost = 0.
+    #         total_batch = int(mnist_data.train.num_examples/batch_size)
+    #         for i in range(total_batch):
+    #             batch_xs, batch_ys = mnist_data.train.next_batch(batch_size)
+    #             sess.run(optimizer_minop, feed_dict={X: batch_xs, Y: batch_ys})
+    #             cost = sess.run(loss, feed_dict={X: batch_xs, Y: batch_ys})
+    #             avg_cost += cost/total_batch
+    #             if i % 20 == 0:
+    #                 print("Epoch:", '%03d' % (epoch+1), "Step:", '%03d' % i,
+    #                       "Loss:", str(cost))
+
+    # --------------------------------------
+    # use trainer class
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(net, Y))
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+    accuracy = tf.reduce_mean(tf.cast(
+        tf.equal(tf.argmax(net, 1), tf.argmax(Y, 1)),
+        tf.float32), name='acc')
 
-    # start
-    init = tf.initialize_all_variables()
+    trainop = tflearn.TrainOp(loss=loss, optimizer=optimizer,
+                              metric=accuracy, batch_size=128)
 
-    with tf.Session() as sess:
-        sess.run(init)
-        batch_size = 128
-        for epoch in range(2):
-            avg_cost = 0.
-            total_batch = int(mnist_data.train.num_examples/batch_size)
-            for i in range(total_batch):
-                batch_xs, batch_ys = mnist_data.train.next_batch(batch_size)
-                sess.run(optimizer, feed_dict={X: batch_xs, Y: batch_ys})
-                cost = sess.run(loss, feed_dict={X: batch_xs, Y: batch_ys})
-                avg_cost += cost/total_batch
-                if i % 20 == 0:
-                    print("Epoch:", '%03d' % (epoch+1), "Step:", '%03d' % i,
-                          "Loss:", str(cost))
+    trainer = tflearn.Trainer(train_ops=trainop, tensorboard_verbose=0)
+    trainer.fit({X: trainX, Y: trainY}, val_feed_dicts={
+                X: testX, Y: testY}, n_epoch=2, show_metric=True)
+    trainer.save('models/mnist_cnn.tfl')
 
 
 with tf.Graph().as_default():
