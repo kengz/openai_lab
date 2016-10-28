b87a1c662a00909208b7643ededc681533a4869f
lgraesser <username@users.noreply.github.com>
2016-10-28 15:35:32 -0400

    Adding alternative nn and updated run_dqn.py

---
+++ rl/keras_dqn_2.py
@@ -0,0 +1,106 @@
+import numpy as np
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.optimizers import SGD
+from keras.regularizers import l1, l2
+from keras.constraints import maxnorm
+from keras.objectives import mse
+
+
+class DQN(object):
+
+    '''
+    The simplest deep Q network. See DQN.md
+    '''
+
+    def __init__(self, env_spec,
+                 gamma=0.95, learning_rate=0.1,
+                 init_e=1.0, final_e=0.1, e_anneal_steps=1000,
+                 batch_size=16, n_epoch=1):
+        self.env_spec = env_spec
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.init_e = init_e
+        self.final_e = final_e
+        self.e = self.init_e
+        self.e_anneal_steps = e_anneal_steps
+        self.batch_size = batch_size
+        self.n_epoch = n_epoch
+        self.build_graph()
+
+    def build_net(self):
+        model = Sequential()
+        # Not clear how much better the algorithm is with regularization
+        model.add(Dense(4,
+                        input_shape=(self.env_spec['state_dim'],),
+                        init='lecun_uniform', activation='sigmoid'))
+        #model.add(Dense(2, init='lecun_uniform', activation='sigmoid'))
+        model.add(Dense(self.env_spec['action_dim'], init='lecun_uniform'))
+        model.summary()
+        self.model = model
+        return model
+
+    def build_graph(self):
+        self.build_net()
+        self.optimizer = SGD(lr=self.learning_rate)
+        self.model.compile(loss='mean_squared_error', optimizer=self.optimizer)
+        print("Model built and compiled")
+        return self.model
+
+    def train(self, replay_memory):
+        '''
+        step 1,2,3,4 of algo. plural for batch's multiple values
+        replay_memory is provided externally
+        '''
+        minibatch = replay_memory.rand_minibatch(self.batch_size)
+        for epoch in range(self.n_epoch):
+            # algo step 1
+            Q_states = self.model.predict(minibatch['states'])
+            # algo step 2
+            Q_next_states = self.model.predict(minibatch['next_states'])
+            # batch x num_actions
+            Q_next_states_max = np.amax(Q_next_states, axis=1)
+            # Q targets for batch-actions a;
+            # with terminal to make future reward 0 if end
+            Q_targets_a = minibatch['rewards'] + self.gamma * \
+                (1 - minibatch['terminals']) * Q_next_states_max
+            # set Q_targets of a as above,
+            # and the non-action units' Q_targets to
+            # as from algo step 1
+            Q_targets = minibatch['actions'] * Q_targets_a[:, np.newaxis] + \
+                (1 - minibatch['actions']) * Q_states
+            loss = self.model.train_on_batch(minibatch['states'], Q_targets)
+        return loss
+
+    def update_e(self):
+        '''
+        strategy to update epsilon
+        '''
+        self.e = max(self.e -
+                     (self.init_e - self.final_e)/float(self.e_anneal_steps),
+                     self.final_e)
+        return self.e
+
+    def select_action(self, state):
+        '''
+        step 1 of algo, feedforward
+        '''
+        if self.e > np.random.rand():
+            action = np.random.choice(self.env_spec['actions'])
+            # hmm maybe flip by ep?
+            #print('random act')
+        else:
+            state = np.reshape(state, (1,4))
+            Q_state = self.model.predict(state)
+            action = np.argmax(Q_state)
+            #print('---')
+        self.update_e()
+        return action
+
+    def save(self, model_path, global_step=None):
+        print('Saving model checkpoint')
+        self.model.save_weights(model_path)
+
+    def restore(self, model_path):
+        self.model.load_weights(model_path, by_name=False)
--- rl/run_dqn.py
+++ rl/run_dqn.py
@@ -2,16 +2,13 @@ import util
 import gym
 from util import *
 from replay_memory import ReplayMemory
-from keras_dqn import DQN
-
-import tensorflow as tf  # to be removed
-
+from keras_dqn_2 import DQN
 
 # rl sys configs, need to implement the required_sys_keys in util
 # only implement constants here,
 # on reset will add vars: {epi, history, mean_rewards, solved}
 sys_vars = {
-    'RENDER': False,
+    'RENDER': True,
     'GYM_ENV_NAME': 'CartPole-v0',
     'SOLVED_MEAN_REWARD': 195.0,
     'MAX_STEPS': 200,
@@ -33,6 +30,8 @@ def run_episode(env, dqn, replay_memory):
     state = env.reset()
     replay_memory.reset_state(state)
     total_rewards = 0
+    print("DQN params: e: {} learning_rate: {} batch size: {} num_epochs: {}".format(dqn.e, 
+                                            dqn.learning_rate, dqn.batch_size, dqn.n_epoch))
 
     for t in range(sys_vars.get('MAX_STEPS')):
         if sys_vars.get('RENDER'):
@@ -42,7 +41,6 @@ def run_episode(env, dqn, replay_memory):
         next_state, reward, done, info = env.step(action)
         replay_memory.add_exp(action, reward, next_state, done)
         dqn.train(replay_memory)
-
         state = next_state
         total_rewards += reward
         if done:
@@ -58,12 +56,16 @@ def run_session(param={}):
     env = gym.make(sys_vars['GYM_ENV_NAME'])
     env_spec = get_env_spec(env)
     replay_memory = ReplayMemory(env_spec)
-    sess = tf.Session()
-    dqn = DQN(env_spec, sess, **param)
+    #sess = tf.Session()
+    #dqn = DQN(env_spec, sess, **param)
+    dqn = DQN(env_spec, **param)
 
     for epi in range(sys_vars['MAX_EPISODES']):
         sys_vars['epi'] = epi
         run_episode(env, dqn, replay_memory)
+        # Best so far, increment num epochs every 2 up to a max of 5
+        if (dqn.n_epoch < 5 and epi % 2 == 0):
+            dqn.n_epoch += 1
         if sys_vars['solved']:
             break
 
@@ -73,8 +75,8 @@ def run_session(param={}):
 if __name__ == '__main__':
     run_session(
         param={'e_anneal_steps': 5000,
-               'learning_rate': 0.1,
-               'n_epoch': 20,
+               'learning_rate': 0.01,
+               'n_epoch': 1,
                'gamma': 0.99})
 
     # # advanced parallel param selection from util
