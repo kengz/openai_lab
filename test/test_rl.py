7efd7b39744fa509ec1b5e45700015c88e52b279
kengz <kengzwl@gmail.com>
2016-11-13 10:04:13 -0500

    fix tests

--- test/test_rl.py
+++ test/test_rl.py
@@ -2,41 +2,41 @@ import unittest
 import pytest
 from os import environ
 environ['CI'] = environ.get('CI') or 'true'
-import rl
+from rl import main
 
 
 class DQNTest(unittest.TestCase):
 
     def test_run_gym_tour(self):
-        sys_vars = rl.main.run('dummy')
+        sys_vars = main.run('dummy')
         assert(sys_vars['RENDER'] == False)
         # ensure it runs, and returns the sys_vars
         assert(isinstance(sys_vars, dict))
         assert(sys_vars['epi'] > 0)
 
     def test_run_q_table(self):
-        sys_vars = rl.main.run('q_table')
+        sys_vars = main.run('q_table')
         assert(sys_vars['RENDER'] == False)
         # ensure it runs, and returns the sys_vars
         assert(isinstance(sys_vars, dict))
         assert(sys_vars['epi'] > 0)
 
     def test_run_dqn(self):
-        sys_vars = rl.main.run('dqn')
+        sys_vars = main.run('dqn')
         assert(sys_vars['RENDER'] == False)
         # ensure it runs, and returns the sys_vars
         assert(isinstance(sys_vars, dict))
         assert(sys_vars['epi'] > 0)
 
     def test_run_dqn(self):
-        sys_vars = rl.main.run('dqn')
+        sys_vars = main.run('dqn')
         assert(sys_vars['RENDER'] == False)
         # ensure it runs, and returns the sys_vars
         assert(isinstance(sys_vars, dict))
         assert(sys_vars['epi'] > 0)
 
     def test_run_double_dqn(self):
-        sys_vars = rl.main.run('double_dqn')
+        sys_vars = main.run('double_dqn')
         assert(sys_vars['RENDER'] == False)
         # ensure it runs, and returns the sys_vars
         assert(isinstance(sys_vars, dict))
