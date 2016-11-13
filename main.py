1e84e65fc9f5c9e0d2bed851de86b466cd789227
kengz <kengzwl@gmail.com>
2016-11-13 17:33:34 -0500

    switch to run mountain car

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from rl.session import sess_specs, run
 
 if __name__ == '__main__':
     # print(list(sess_specs.keys()))
-    run('dqn')
+    # ['dummy', 'q_table', 'lunar_double_dqn', 'mountain_double_dqn', 'lunar_dqn', 'double_dqn', 'dqn']
+    run('mountain_double_dqn')
