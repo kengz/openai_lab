710c32152a4ea44952b766dcfdb4b20ac2909404
kengz <kengzwl@gmail.com>
2017-01-15 16:28:00 -0500

    fix tests

--- test/test_rl.py
+++ test/test_rl.py
@@ -8,44 +8,20 @@ from rl.experiment import run
 class DQNTest(unittest.TestCase):
 
     def test_run_gym_tour(self):
-        sys_vars = run('dummy')['sys_vars_array'][0]
-        assert(sys_vars['RENDER'] == False)
-        # ensure it runs, and returns the sys_vars
-        assert(isinstance(sys_vars, dict))
-        assert(sys_vars['epi'] > 0)
+        metrics_df = run('dummy')
 
     def test_run_q_table(self):
-        sys_vars = run('q_table')['sys_vars_array'][0]
-        assert(sys_vars['RENDER'] == False)
-        # ensure it runs, and returns the sys_vars
-        assert(isinstance(sys_vars, dict))
-        assert(sys_vars['epi'] > 0)
+        metrics_df = run('q_table')
 
     def test_run_dqn(self):
-        sys_vars = run('dqn')['sys_vars_array'][0]
-        assert(sys_vars['RENDER'] == False)
-        # ensure it runs, and returns the sys_vars
-        assert(isinstance(sys_vars, dict))
-        assert(sys_vars['epi'] > 0)
+        metrics_df = run('dqn')
 
     def test_run_double_dqn(self):
-        sys_vars = run('double_dqn')['sys_vars_array'][0]
-        assert(sys_vars['RENDER'] == False)
-        # ensure it runs, and returns the sys_vars
-        assert(isinstance(sys_vars, dict))
-        assert(sys_vars['epi'] > 0)
+        metrics_df = run('double_dqn')
 
     def test_run_mountain_double_dqn(self):
-        sys_vars = run('mountain_double_dqn')['sys_vars_array'][0]
-        assert(sys_vars['RENDER'] == False)
-        # ensure it runs, and returns the sys_vars
-        assert(isinstance(sys_vars, dict))
-        assert(sys_vars['epi'] > 0)
+        metrics_df = run('mountain_double_dqn')
 
     # def test_run_all(self):
     #     for x in game_specs.keys():
-    #         sys_vars = run(x)['sys_vars_array'][0]
-    #         assert(sys_vars['RENDER'] == False)
-    #         # ensure it runs, and returns the sys_vars
-    #         assert(isinstance(sys_vars, dict))
-    #         assert(sys_vars['epi'] > 0)
+    #         metrics_df = run(x)
