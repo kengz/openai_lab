9febad81606c0442d6c3889a149aea2e8a15f744
kengz <kengzwl@gmail.com>
2017-01-15 22:01:05 -0500

    default run to lunar_dqn param select, make npm start -b

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from rl.experiment import run
 if __name__ == '__main__':
     # ['dummy', 'q_table', 'lunar_double_dqn',
     #     'mountain_double_dqn', 'lunar_dqn', 'double_dqn', 'dqn']
-    run('dev_dqn', times=2, param_selection=False)
+    # run('dev_dqn', times=2, param_selection=False)
     # run('dqn', times=2, param_selection=False)
     # run('CartPole-v0_DQN_LinearMemoryWithForgetting_BoltzmannPolicy_2017-01-14_201002_e1', plot_only=True)
-    # run('lunar_dqn', times=5, param_selection=True, line_search=True)
+    run('lunar_dqn', times=3, param_selection=True, line_search=True)
