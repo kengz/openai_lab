fefec8bd5b8f0db3743ed1f8153d17eb8c86dfa0
kengz <kengzwl@gmail.com>
2017-01-18 08:15:31 -0500

    fix indentation and import in preprocessing

--- rl/state_preprocessing.py
+++ rl/state_preprocessing.py
@@ -1,41 +1,67 @@
-from util import logger
 import numpy as np
 import scipy as sp
+from rl.util import logger
+
+
+# Util functions for state preprocessing
+
+def resize_image(im):
+    return sp.misc.imresize(im, (110, 84))
+
+
+def crop_image(im):
+    return im[-84:, :]
+
+
+def process_image_atari(im):
+    # Image preprocessing from the paper
+    # Playing Atari with Deep Reinforcement Learning, 2013
+    # Takes an RGB image and converts it to grayscale,
+    # downsizes to 110 x 84
+    # and crops to square 84 x 84, taking bottomost rows of image
+    im_gray = np.dot(im[..., :3], [0.299, 0.587, 0.114])
+    im_resized = resize_image(im_gray)
+    im_cropped = crop_image(im_resized)
+    return im_cropped
+
 
 class PreProcessing(object):
 
     '''
     The Base class for state preprocessing
     '''
-    def __init__(self, 
-                        **kwargs):  # absorb generic param without breaking
+
+    def __init__(self,
+                 **kwargs):  # absorb generic param without breaking
         '''
         Construct externally, and set at Agent.compile()
         '''
         self.agent = None
 
     def preprocessing_action_sel(self, state,
-                                                                    previous_state,
-                                                                    pre_previous_state,
-                                                                    pre_pre_previous_state):
+                                 previous_state,
+                                 pre_previous_state,
+                                 pre_pre_previous_state):
         raise NotImplementedError()
 
     def preprocessing_memory(self, temp_exp_mem, t):
         raise NotImplementedError()
 
+
 class NoPreProcessing(PreProcessing):
+
     '''
     Default class, no preprocessing
     '''
 
     def __init__(self,
-                        **kwargs):  # absorb generic param without breaking):
+                 **kwargs):  # absorb generic param without breaking):
         super(NoPreProcessing, self).__init__()
 
     def preprocessing_action_sel(self, state,
-                                                                    previous_state,
-                                                                    pre_previous_state,
-                                                                    pre_pre_previous_state):
+                                 previous_state,
+                                 pre_previous_state,
+                                 pre_pre_previous_state):
         return state
 
     def preprocessing_memory(self, temp_exp_mem, t):
@@ -46,21 +72,23 @@ class NoPreProcessing(PreProcessing):
         next_state = temp_exp_mem[-1][3]
         done = temp_exp_mem[-1][4]
         self.agent.memory.add_exp_processed(state, action, reward,
-                                       next_state, next_state, done)
+                                            next_state, next_state, done)
 
 
 class StackStates(PreProcessing):
+
     '''
     Current and last state are concatenated to form input to model
     '''
+
     def __init__(self,
-                        **kwargs):  # absorb generic param without breaking):
+                 **kwargs):  # absorb generic param without breaking):
         super(StackStates, self).__init__()
 
     def preprocessing_action_sel(self, state,
-                                                                    previous_state,
-                                                                    pre_previous_state,
-                                                                    pre_pre_previous_state):
+                                 previous_state,
+                                 pre_previous_state,
+                                 pre_pre_previous_state):
         return np.concatenate([previous_state, state])
 
     def preprocessing_memory(self, temp_exp_mem, t):
@@ -76,23 +104,27 @@ class StackStates(PreProcessing):
             done = temp_exp_mem[-1][4]
             if (t == 1):
                 logger.debug("State shape: {}".format(processed_state.shape))
-                logger.debug("Next state shape: {}".format(processed_next_state.shape))
-            self.agent.memory.add_exp_processed(processed_state, action, reward,
-                                           processed_next_state, next_state, done)
+                logger.debug(
+                    "Next state shape: {}".format(processed_next_state.shape))
+            self.agent.memory.add_exp_processed(
+                processed_state, action, reward,
+                processed_next_state, next_state, done)
 
 
 class DiffStates(PreProcessing):
+
     '''
     Different between current and last state is input to model
     '''
+
     def __init__(self,
-                        **kwargs):  # absorb generic param without breaking):
+                 **kwargs):  # absorb generic param without breaking):
         super(DiffStates, self).__init__()
 
     def preprocessing_action_sel(self, state,
-                                                                    previous_state,
-                                                                    pre_previous_state,
-                                                                    pre_pre_previous_state):
+                                 previous_state,
+                                 pre_previous_state,
+                                 pre_pre_previous_state):
         return state - previous_state
 
     def preprocessing_memory(self, temp_exp_mem, t):
@@ -106,11 +138,15 @@ class DiffStates(PreProcessing):
             done = temp_exp_mem[-1][4]
             if (t == 1):
                 logger.debug("State shape: {}".format(processed_state.shape))
-                logger.debug("Next state shape: {}".format(processed_next_state.shape))
-            self.agent.memory.add_exp_processed(processed_state, action, reward,
-                                           processed_next_state, next_state, done)
+                logger.debug(
+                    "Next state shape: {}".format(processed_next_state.shape))
+            self.agent.memory.add_exp_processed(
+                processed_state, action, reward,
+                processed_next_state, next_state, done)
+
 
 class Atari(PreProcessing):
+
     '''
     Convert images to greyscale, downsize, crop, then stack 4 states
     NOTE: Image order is cols * rows * channels to match openai gym format
@@ -118,17 +154,17 @@ class Atari(PreProcessing):
     '''
 
     def __init__(self,
-                        **kwargs):  # absorb generic param without breaking):
+                 **kwargs):  # absorb generic param without breaking):
         super(Atari, self).__init__()
 
     def preprocessing_action_sel(self, state,
-                                                                    previous_state,
-                                                                    pre_previous_state,
-                                                                    pre_pre_previous_state):
+                                 previous_state,
+                                 pre_previous_state,
+                                 pre_pre_previous_state):
         arrays = (process_image_atari(state),
-              process_image_atari(previous_state),
-              process_image_atari(pre_previous_state),
-              process_image_atari(pre_pre_previous_state))
+                  process_image_atari(previous_state),
+                  process_image_atari(pre_previous_state),
+                  process_image_atari(pre_pre_previous_state))
         return np.stack(arrays, axis=-1)
 
     def preprocessing_memory(self, temp_exp_mem, t):
@@ -149,26 +185,8 @@ class Atari(PreProcessing):
             done = temp_exp_mem[-1][4]
             if (t == 3):
                 logger.debug("State shape: {}".format(processed_state.shape))
-                logger.debug("Next state shape: {}".format(processed_next_state.shape))
-            self.agent.memory.add_exp_processed(processed_state, action, reward,
-                                           processed_next_state, next_state, done)
-
-
-# Util functions for state preprocessing
-
-def resize_image(im):
-    return sp.misc.imresize(im, (110, 84))
-
-def crop_image(im):
-    return im[-84:, :]
-
-def process_image_atari(im):
-    # Image preprocessing from the paper
-    # Playing Atari with Deep Reinforcement Learning, 2013
-    # Takes an RGB image and converts it to grayscale,
-    # downsizes to 110 x 84
-    # and crops to square 84 x 84, taking bottomost rows of image
-    im_gray = np.dot(im[..., :3], [0.299, 0.587, 0.114])
-    im_resized = resize_image(im_gray)
-    im_cropped = crop_image(im_resized)
-    return im_cropped
+                logger.debug(
+                    "Next state shape: {}".format(processed_next_state.shape))
+            self.agent.memory.add_exp_processed(
+                processed_state, action, reward,
+                processed_next_state, next_state, done)
