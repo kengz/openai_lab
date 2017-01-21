5734279a63be8182e5d24e588236620e14a75d50
kengz <kengzwl@gmail.com>
2017-01-21 18:19:19 -0500

    update gulp for new data folder system

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const source = './data'
 const destination = '/keybase/private/kengz,lgraesser/data';
 
 gulp.task('default', function() {
-  gulp.src(source + '/*', { base: source })
+  gulp.src(source + '/**/*', { base: source })
     .pipe(watch(source, { base: source }))
     .pipe(changed(destination))
     .pipe(gulp.dest(destination));
