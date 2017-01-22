ed20b4a50373a9990091ae4c86ad66d329491116
kengz <kengzwl@gmail.com>
2017-01-21 19:33:32 -0500

    quick fix for atari agent, still break on parallel runs

--- rl/agent/atari_conv_dqn.py
+++ rl/agent/atari_conv_dqn.py
@@ -64,12 +64,17 @@ class ConvDQN(DQN):
         logger.info("Model built and compiled")
         return self.model
 
-    def recompile_model(self, new_lr):
+    def recompile_model(self, params, sys_vars):
         '''
         Option to change model optimizer settings
         Doesn't do anything at the moment
         '''
-        self.learning_rate = new_lr
-        self.optimizer = RMSprop()
-        self.model.compile(loss='mean_squared_error', optimizer=self.optimizer)
-        logger.info("Model recompiled but optimizer set to RMSProp so it didn't do anything")
+        if 'epi_change_learning_rate' in params:
+            if (sys_vars['epi'] == params['epi_change_learning_rate']):
+                self.learning_rate = self.learning_rate / 10.0
+                self.optimizer = RMSprop(lr=self.learning_rate)
+                self.model.compile(
+                    loss='mean_squared_error', optimizer=self.optimizer)
+                logger.info('Model recompiled with new settings: '
+                            'Learning rate: {}'.format(self.learning_rate))
+        return self.model
