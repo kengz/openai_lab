5da41fdbb8b7e9d08744df3e5187d12da277636e
kengz <kengzwl@gmail.com>
2017-01-22 10:39:43 -0500

    refactor lunar dqn

--- rl/agent/lunar_dqn.py
+++ rl/agent/lunar_dqn.py
@@ -1,27 +1,10 @@
 from rl.agent.dqn import DQN
-from rl.util import logger
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.optimizers import SGD
 
 
 class LunarDQN(DQN):
 
     def __init__(self, *args, **kwargs):
         super(LunarDQN, self).__init__(*args, **kwargs)
-
-    def build_model(self):
-        model = Sequential()
-        self.build_hidden_layers(model)
-        model.add(Dense(self.env_spec['action_dim'], init='lecun_uniform'))
-        logger.info("Model summary")
-        model.summary()
-        self.model = model
-
-        self.optimizer = SGD(lr=self.learning_rate)
-        self.model.compile(loss='mean_squared_error', optimizer=self.optimizer)
-        logger.info("Model built and compiled")
-        return self.model
 
     def update_n_epoch(self, sys_vars):
         '''
