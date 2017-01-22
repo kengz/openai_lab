faffe71929eb08e777dad8359f4630f8c3666432
kengz <kengzwl@gmail.com>
2017-01-22 11:13:38 -0500

    refactor double_dqn compute Q method

--- rl/agent/double_dqn.py
+++ rl/agent/double_dqn.py
@@ -23,37 +23,30 @@ class DoubleDQN(DQN):
         logger.info("Models built and compiled")
         return self.model, self.model2
 
+    def compute_Q_states(self, minibatch):
+        Q_states = self.model.predict(minibatch['states'])
+        # Different from (single) dqn: Select max using model 2
+        Q_next_states_select = self.model2.predict(
+            minibatch['next_states'])
+        Q_next_states_max_ind = np.argmax(Q_next_states_select, axis=1)
+        # if more than one max, pick 1st
+        if (Q_next_states_max_ind.shape[0] > 1):
+            Q_next_states_max_ind = Q_next_states_max_ind[0]
+
+        # same as dqn again, but use Q_next_states_max_ind above
+        Q_next_states = self.model.predict(minibatch['next_states'])
+        Q_next_states_max = Q_next_states[:, Q_next_states_max_ind]
+        return (Q_states, Q_next_states_max)
+
     def train(self, sys_vars):
-        '''
-        Training is for the Q function (NN) only
-        otherwise (e.g. policy) see self.update()
-        step 1,2,3,4 of algo.
-        '''
         loss_total = 0
         for _epoch in range(self.n_epoch):
+            # same as dqn
             minibatch = self.memory.rand_minibatch(self.batch_size)
-            # note the computed values below are batched in array
-            Q_states = self.model.predict(minibatch['states'])
 
-            # Select max using model 2
-            Q_next_states_select = self.model2.predict(
-                minibatch['next_states'])
-            Q_next_states_max_ind = np.argmax(Q_next_states_select, axis=1)
-            # if more than one max, pick 1st
-            if (Q_next_states_max_ind.shape[0] > 1):
-                Q_next_states_max_ind = Q_next_states_max_ind[0]
-            # Evaluate max using model 1
-
-            Q_next_states = self.model.predict(minibatch['next_states'])
-            Q_next_states_max = Q_next_states[:, Q_next_states_max_ind]
-
-            # make future reward 0 if exp is terminal
-            Q_targets_a = minibatch['rewards'] + self.gamma * \
-                (1 - minibatch['terminals']) * Q_next_states_max
-            # set batch Q_targets of a as above, the rest as is
-            # minibatch['actions'] is one-hot encoded
-            Q_targets = minibatch['actions'] * Q_targets_a[:, np.newaxis] + \
-                (1 - minibatch['actions']) * Q_states
+            (Q_states, Q_next_states_max) = self.compute_Q_states(minibatch)
+            Q_targets = self.compute_Q_targets(
+                minibatch, Q_states, Q_next_states_max)
 
             loss = self.model.train_on_batch(minibatch['states'], Q_targets)
             loss_total += loss
