7e1f971c3bb653e37d8a59da08856a4ea6c973eb
kengz <kengzwl@gmail.com>
2017-01-26 22:34:06 -0500

    integrate -g flag into plotter init and plot method

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from util import args
 if __name__ == '__main__':
     # Defaults to dev_dqn, run once, with no param selection
     run(args.sess_to_run,
-            args.times,
-            args.param_selection,
-            args.line_search)
+        args.times,
+        args.param_selection,
+        args.line_search)
