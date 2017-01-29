1159105de9f32894970c303e09ed39847cf91f5b
lgraesser <lhgraesser@gmail.com>
2017-01-29 09:55:48 -0500

    Adding DoubleDQN Boltzmann policy

--- rl/policy.py
+++ rl/policy.py
@@ -183,7 +183,7 @@ class BoltzmannPolicy(Policy):
         assert Q_state.ndim == 1
         Q_state = Q_state.astype('float64')  # fix precision nan issue
         Q_state = Q_state - np.amax(Q_state)  # prevent overflow
-        exp_values = np.exp(Q_state / self.tau) + 0.0001  # prevent underflow
+        exp_values = np.exp(Q_state / self.tau) + 0.001  # prevent underflow
         assert not np.isnan(exp_values).any()
         probs = np.array(exp_values / np.sum(exp_values))
         probs /= probs.sum()  # renormalize to prevent floating pt error
@@ -197,3 +197,29 @@ class BoltzmannPolicy(Policy):
         slope = rise / float(self.exploration_anneal_episodes)
         self.tau = max(slope * epi + self.init_tau, self.final_tau)
         return self.tau
+
+class DoubleDQNBoltzmannPolicy(BoltzmannPolicy):
+    '''
+    Same as the Boltzmann policy but for a Double DQN agent
+    '''
+
+    def __init__(self,
+                 init_tau=5., final_tau=0.5, exploration_anneal_episodes=20,
+                 **kwargs):  # absorb generic param without breaking
+        super(DoubleDQNBoltzmannPolicy, self).__init__()
+
+    def select_action(self, state):
+        agent = self.agent
+        state = np.reshape(state, (1, state.shape[0]))
+        Q_state1 = agent.model.predict(state)[0]  # extract from batch predict
+        Q_state2 = agent.model2.predict(state)[0]  # extract from batch predict
+        Q_state = Q_state1 + Q_state2
+        assert Q_state.ndim == 1
+        Q_state = Q_state.astype('float64')  # fix precision nan issue
+        Q_state = Q_state - np.amax(Q_state)  # prevent overflow
+        exp_values = np.exp(Q_state / self.tau) + 0.001  # prevent underflow
+        assert not np.isnan(exp_values).any()
+        probs = np.array(exp_values / np.sum(exp_values))
+        probs /= probs.sum()  # renormalize to prevent floating pt error
+        action = np.random.choice(agent.env_spec['actions'], p=probs)
+        return action
