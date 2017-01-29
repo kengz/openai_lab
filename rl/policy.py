20e552878a9b3b1cd43062aca6f3c3d708e08da1
lgraesser <lhgraesser@gmail.com>
2017-01-28 19:39:57 -0500

    Fixing double dqn algorithm

--- rl/policy.py
+++ rl/policy.py
@@ -57,6 +57,35 @@ class EpsilonGreedyPolicy(Policy):
         self.e = max(slope * epi + self.init_e, self.final_e)
         return self.e
 
+class DoubleDQNPolicy(EpsilonGreedyPolicy):
+    '''
+    Policy to accompany double dqn agents
+    When actions are not random this policy
+    selects actions by symming the outputs from
+    each of the two Q-state approximators
+    before taking the max of the result
+    '''
+
+    def __init__(self,
+                 init_e=1.0, final_e=0.1, exploration_anneal_episodes=30,
+                 **kwargs):  # absorb generic param without breaking
+        super(DoubleDQNPolicy, self).__init__()
+
+    def select_action(self, state):
+        '''epsilon-greedy method'''
+        agent = self.agent
+        if self.e > np.random.rand():
+            action = np.random.choice(agent.env_spec['actions'])
+        else:
+            state = np.reshape(state, (1, state.shape[0]))
+            # extract from batch predict
+            Q_state1 = agent.model.predict(state)[0]
+            Q_state2 = agent.model.predict(state)[0]
+            Q_state = Q_state1 + Q_state2
+            assert Q_state.ndim == 1
+            action = np.argmax(Q_state)
+        return action
+
 class DecayingEpsilonGreedyPolicy(EpsilonGreedyPolicy):
     '''
     Inspired by alvacarce's solution to mountain car
