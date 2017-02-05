783e2e34e1b5a1b18049b4d577c4f20e8ee4e529
kengz <kengzwl@gmail.com>
2017-02-04 22:33:32 -0500

    tweak agent import structure to expose class directly, update sess_spec to remove dot notation

--- rl/agent/__init__.py
+++ rl/agent/__init__.py
@@ -1,12 +1,27 @@
-# curse of python pathing, hack to solve rel import
-import sys
-from os import path
-file_path = path.normpath(path.join(path.dirname(__file__)))
-sys.path.insert(0, file_path)
+def _import_package_files():
+    """ Dynamically import all the public attributes of the python modules in this
+        file's directory (the package directory) and return a list of their names.
+    """
+    import os
+    import inspect
+    exports = []
+    globals_, locals_ = globals(), locals()
+    package_path = os.path.dirname(__file__)
+    package_name = os.path.basename(package_path)
 
-# another py curse, expose to prevent 'agent.<agent>' call
-from os.path import dirname, basename, isfile
-import glob
-pattern = "/*.py"
-modules = glob.glob(dirname(__file__)+pattern)
-__all__ = [basename(f)[:-3] for f in modules if isfile(f)]
+    for filename in os.listdir(package_path):
+        modulename, ext = os.path.splitext(filename)
+        if modulename[0] != '_' and ext in ('.py', '.pyw'):
+            subpackage = '{}.{}'.format(
+                package_name, modulename)  # pkg relative
+            module = __import__(subpackage, globals_, locals_, [modulename])
+            modict = module.__dict__
+            names = (modict['__all__'] if '__all__' in modict else
+                     [name for name in
+                      modict if inspect.isclass(modict[name])])  # all public
+            exports.extend(names)
+            globals_.update((name, modict[name]) for name in names)
+
+    return exports
+
+__all__ = ['__all__'] + _import_package_files()
