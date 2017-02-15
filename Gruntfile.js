2a1867fe9135c1ea9e6e7105a57e72227917a519
kengz <kengzwl@gmail.com>
2017-02-15 12:40:39 -0500

    correct regex to capture experiment name or id

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const resolve = require('resolve-dir')
 
 
-// generic experimentId matcher. index 2: experimentId, 3: experimentName
-const expIdRegex = /(\-e\s+)?(([a-zA-Z0-9_]+)_\d{4}\-\d{2}\-\d{2}_\d{6})/
+// generic experimentId matcher. index 2: experimentId, 3 or 4: experimentName
+const expIdRegex = /(\-e\s+)?(([a-zA-Z0-9_]+)_\d{4}\-\d{2}\-\d{2}_\d{6}|([a-zA-Z0-9_]+))/
 const historyPath = './config/history.json'
 const finishMsg = `
 ===========================================
@@ -22,20 +22,6 @@ module.exports = function(grunt) {
   const experimentTasks = _.map(experiments, function(name) {
     return `shell:exp:${name}`
   })
-
-  function remoteCmd() {
-    return (grunt.option('remote') || grunt.option('r')) ? 'xvfb-run -a -s "-screen 0 1400x900x24" --' : ''
-  }
-
-  function notiCmd(experiment) {
-    return grunt.option('prod') ? `NOTI_SLACK_DEST='${config.NOTI_SLACK_DEST}' NOTI_SLACK_TOK='${config.NOTI_SLACK_TOK}' noti -k -t 'Experiment completed' -m '[${new Date().toISOString()}] ${experiment} on ${process.env.USER}'` : ''
-  }
-
-  function composeCommand(experiment) {
-    // override with custom command if has 'python'
-    var cmd = _.includes(experiment, 'python') ? experiment : `python3 main.py -bgp -e ${experiment} -t 5`
-    return `${remoteCmd()} ${cmd} | tee -a ./data/terminal.log; ${notiCmd(experiment)}`
-  }
 
   function readHistory() {
     if (grunt.option('resume')) {
@@ -55,18 +41,37 @@ module.exports = function(grunt) {
   let history = readHistory()
 
   function writeHistory(history) {
-    fs.writeFile(historyPath, JSON.stringify(history, null, 4))
+    fs.writeFile(historyPath, JSON.stringify(history, null, 2))
   }
 
   function updateHistory(filepath) {
     const matchedPath = filepath.match(expIdRegex)
     if (matchedPath) {
       const experimentId = matchedPath[2]
-      const experimentName = matchedPath[3]
+      const experimentName = matchedPath[3] || matchedPath[4]
       history[experimentName] = experimentId
       writeHistory(history)
     }
   }
+
+  function remoteCmd() {
+    return (grunt.option('remote') || grunt.option('r')) ? 'xvfb-run -a -s "-screen 0 1400x900x24" --' : ''
+  }
+
+  function notiCmd(experiment) {
+    return grunt.option('prod') ? `NOTI_SLACK_DEST='${config.NOTI_SLACK_DEST}' NOTI_SLACK_TOK='${config.NOTI_SLACK_TOK}' noti -k -t 'Experiment completed' -m '[${new Date().toISOString()}] ${experiment} on ${process.env.USER}'` : ''
+  }
+
+  function composeCommand(experiment) {
+    if (grunt.option('resume')) {
+      // search and replace using history
+      // but if preinit hmm history is empty, so just dont replace
+    }
+    // override with custom command if has 'python'
+    var cmd = _.includes(experiment, 'python') ? experiment : `python3 main.py -bgp -e ${experiment} -t 5`
+    return `${remoteCmd()} ${cmd} | tee -a ./data/terminal.log; ${notiCmd(experiment)}`
+  }
+
 
 
   require('load-grunt-tasks')(grunt)
