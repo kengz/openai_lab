0454d93d5649603043c99acbb7d1f5dbb6d49266
lgraesser <lhgraesser@gmail.com>
2017-02-16 16:50:24 -0500

    Adding Adam optimizer

--- rl/optimizer/Adam.py
+++ rl/optimizer/Adam.py
@@ -1,6 +1,8 @@
+from rl.util import *
+from rl.optimizer.base_optimizer import Optimizer
 from keras.optimizers import Adam
 
-class Adam(Optimizer):
+class Adam_L(Optimizer):
     '''
     Adam optimizer
     Potential params:
@@ -12,11 +14,49 @@ class Adam(Optimizer):
         Suggested to leave at default params with the expected of lr
     '''
 
-    def __init__(self, **kwargs):  # absorb generic param without breaking
-        super(Adam, self).__init__()
+    def __init__(self, param):  
+        super(Adam_L, self).__init__(param)
+        self.set_init_optim_params()
+        self.init_optimizer()
+        logger.info("Adam optimizer initialized. Params: {}".format(self.optim_param))
 
-    def init_optimizer():
-        pass
+    def set_init_optim_params(self):
+        optim_param = {}
+        if 'learning_rate' in self.param:
+            lr = self.param['learning_rate']
+            optim_param['lr'] = lr
+        if 'beta_1' in self.param:
+            beta_1 = self.param['beta_1']
+            optim_param['beta_1'] = beta_1
+        if 'beta_2' in self.param:
+            beta_2 = self.param['beta_2']
+            optim_param['beta_2'] = beta_2
+        if 'epsilon' in self.param:
+            epsilon = self.param['epsilon']
+            optim_param['epsilon'] = epsilon
+        if 'decay' in self.param:
+            decay = self.param['decay']
+            optim_param['decay'] = decay
+        self.optim_param = optim_param
+
+    def init_optimizer(self):
+        self.optimizer = Adam(**self.optim_param)
 
     def change_optim_params(self, new_params):
-        pass
+        if 'learning_rate' in new_params:
+            lr = new_params['learning_rate']
+            self.optim_param['lr'] = lr
+        if 'beta_1' in new_params:
+            beta_1 = new_params['beta_1']
+            self.optim_param['beta_1'] = momentum
+        if 'beta_2' in new_params:
+            beta_2 = new_params['beta_2']
+            self.optim_param['beta_2'] = beta_2
+        if 'epsilon' in new_params:
+            epsilon = new_params['epsilon']
+            self.optim_param['epsilon'] = epsilon
+        if 'decay' in new_params:
+            decay = new_params['decay']
+            self.optim_param['decay'] = decay
+        self.init_optimizer()
+        logger.info("Adam optimizer parameters changed. New params: {}".format(self.optim_param))
