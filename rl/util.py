362ba618b41f03d2fed674709ac7cf4ebf1b4f7e
kengz <kengzwl@gmail.com>
2017-02-17 17:13:26 -0500

    refactor __init__ import_package_files method into util

--- rl/util.py
+++ rl/util.py
@@ -1,5 +1,6 @@
 import argparse
 import collections
+import inspect
 import json
 import logging
 import multiprocessing as mp
@@ -223,6 +224,32 @@ def get_module(GREF, dot_path):
     for deeper_path in path_arr:
         mod = getattr(mod, deeper_path)
     return mod
+
+
+def import_package_files(globals_, locals_, __file__):
+    '''
+    Dynamically import all the public attributes of the python modules in this
+    file's directory (the package directory) and return a list of their names.
+    '''
+    exports = []
+    # globals_, locals_ = globals(), locals()
+    package_path = os.path.dirname(__file__)
+    package_name = os.path.basename(package_path)
+
+    for filename in os.listdir(package_path):
+        modulename, ext = os.path.splitext(filename)
+        if modulename[0] != '_' and ext in ('.py', '.pyw'):
+            subpackage = '{}.{}'.format(
+                package_name, modulename)  # pkg relative
+            module = __import__(subpackage, globals_, locals_, [modulename])
+            modict = module.__dict__
+            names = (modict['__all__'] if '__all__' in modict else
+                     [name for name in
+                      modict if inspect.isclass(modict[name])])  # all public
+            exports.extend(names)
+            globals_.update((name, modict[name]) for name in names)
+
+    return exports
 
 
 def clean_id_str(id_str):
