be72dfcdfd07854b82855ace3a8b18696089a518
lgraesser <lhgraesser@gmail.com>
2017-02-26 22:04:55 -0500

    Adding more tests, covering all components

--- test/test_rl.py
+++ test/test_rl.py
@@ -3,9 +3,18 @@ import pandas as pd
 from os import environ
 environ['CI'] = environ.get('CI') or 'true'
 from rl.experiment import run
-
+from rl.util import *
+import os
+import sys
+import json
 
 class DQNTest(unittest.TestCase):
+
+    PATH = os.path.dirname(__file__)
+    PROBLEMS = json.loads(open(
+    path.join(PATH, 'test_problems.json')).read())
+    EXPERIMENT_SPECS = json.loads(open(
+    path.join(PATH, 'test_experiment_specs.json')).read())
 
     @classmethod
     def test_gym_tour(cls):
@@ -23,8 +32,53 @@ class DQNTest(unittest.TestCase):
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
-    def test_double_dqn(cls):
+    def test_dqn(cls):
+        data_df = run('dqn')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_dqn(cls):
         data_df = run('double_dqn')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_dqn(cls):
+        data_df = run('dqn_freeze')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_dqn(cls):
+        data_df = run('sarsa')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_dqn(cls):
+        data_df = run('sarsa_exp')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_dqn(cls):
+        data_df = run('sarsa_offpol')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_double_dqn(cls):
+        data_df = run('mountain_dqn')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_double_dqn(cls):
+        data_df = run('lunar_dqn')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_double_dqn(cls):
+        data_df = run('conv_test')
+        assert isinstance(data_df, pd.DataFrame)
+
+    @classmethod
+    def test_double_dqn(cls):
+        data_df = run('conv_test_2')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
