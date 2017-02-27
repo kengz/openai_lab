41c4b85c0e61a0209d2ac3ebe1ca305ea9c896d5
lgraesser <lhgraesser@gmail.com>
2017-02-26 23:12:39 -0500

    Updating test_rl

--- test/test_rl.py
+++ test/test_rl.py
@@ -22,62 +22,62 @@ class DQNTest(unittest.TestCase):
 
     @classmethod
     def test_q_table(cls):
-        data_df = run('q_table')
+        data_df = run('test_q_table')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_dqn(cls):
-        data_df = run('dqn')
+        data_df = run('test_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_double_dqn(cls):
-        data_df = run('double_dqn')
+        data_df = run('test_double_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_dqn_freeze(cls):
-        data_df = run('dqn_freeze')
+        data_df = run('test_dqn_freeze')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_sarsa(cls):
-        data_df = run('sarsa')
+        data_df = run('test_sarsa')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_sarsa_exp(cls):
-        data_df = run('sarsa_exp')
+        data_df = run('test_sarsa_exp')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_sarsa_offpol(cls):
-        data_df = run('sarsa_offpol')
+        data_df = run('test_sarsa_offpol')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_mountain_dqn(cls):
-        data_df = run('mountain_dqn')
+        data_df = run('test_mountain_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_lunar_dqn(cls):
-        data_df = run('lunar_dqn')
+        data_df = run('test_lunar_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_conv_dqn(cls):
-        data_df = run('conv_dqn')
+        data_df = run('test_conv_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_double_conv_dqn(cls):
-        data_df = run('double_conv_dqn')
+        data_df = run('test_double_conv_dqn')
         assert isinstance(data_df, pd.DataFrame)
 
     @classmethod
     def test_dev_dqn_pass(cls):
-        data_df = run('test_dqn')
+        data_df = run('test_test_dqn')
         max_total_rewards = data_df['max_total_rewards_stats_mean'][0]
         print(max_total_rewards)
         assert max_total_rewards > 50
