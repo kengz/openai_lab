4836ee50b1b4cfc5e099dbb20a7131af5f75bf72
kengz <kengzwl@gmail.com>
2017-03-04 14:46:47 -0500

    implement bijectors for random search

--- rl/hyperoptimizer/random_search.py
+++ rl/hyperoptimizer/random_search.py
@@ -13,48 +13,12 @@ class RandomSearch(HyperOptimizer):
         ]
         super(RandomSearch, self).set_keys(**kwargs)
 
-    # def init_sampler(self, real_param):
-    #     '''
-    #     e.g. for 'lr': {real_param}
-    #     where {real_param} = {
-    #         "uniform": {
-    #             "low": 0,
-    #             "high": 1
-    #         }
-    #     }
-    #     '''
-    #     keys = list(real_param.keys())
-    #     assert len(keys) == 1
-    #     dist = keys[0]
-    #     dist_kwargs = real_param[dist]
-    #     return partial(getattr(np.random, dist), **dist_kwargs)
-
     @classmethod
     def sample_hypersphere(cls, dim, r=1):
-        '''Marsaglias algo for sampling uniformly on hypersphere'''
+        '''Marsaglia algo for sampling uniformly on a hypersphere'''
         v = np.random.randn(dim)
         v = v * r / np.linalg.norm(v)
         return v
-
-    def unit_cube_bijection(self):
-        x_vec = self.sample_hypersphere(self.param_space_dims)
-        return {
-            self.param_range_keys[i]: self.biject_axis(
-                x_vec[i],
-                self.param_range[self.param_range_keys[i]])
-            for i in range(self.param_space_dims)
-        }
-
-    def biject_axis(self, norm_val, axis_spec):
-        print('axis spec')
-        print('axis spec')
-        print(axis_spec)
-        if isinstance(axis_spec, list):  # discrete
-            return self.biject_discrete(norm_val, axis_spec)
-        else:  # cont
-            return self.biject_continuous(
-                norm_val, axis_spec['min'], axis_spec['max'])
-        return
 
     # biject [0, 1] to [x_min, x_max]
     @classmethod
@@ -63,11 +27,27 @@ class RandomSearch(HyperOptimizer):
 
     # biject [0, 1] to x_list = [a, b, c, ...] by binning
     def biject_discrete(self, norm_val, x_list):
-        x_len = len(x_list)
-        inds = np.arange(x_len)
-        cont_val = self.biject_continuous(norm_val, 0, x_len)
+        list_len = len(x_list)
+        inds = np.arange(list_len)
+        cont_val = self.biject_continuous(norm_val, 0, list_len)
         ind = np.digitize(cont_val, inds) - 1
         return x_list[ind]
+
+    def biject_dim(self, norm_val, dim_spec):
+        if isinstance(dim_spec, list):  # discrete
+            return self.biject_discrete(norm_val, dim_spec)
+        else:  # cont
+            return self.biject_continuous(
+                norm_val, dim_spec['min'], dim_spec['max'])
+        return
+
+    # biject a vector on unit cube into param_space
+    def biject_unit_cube(self, v):
+        param_space_v = {}
+        for i, param_key in enumerate(self.param_range_keys):
+            dim_spec = self.param_range[param_key]
+            param_space_v[param_key] = self.biject_dim(v[i], dim_spec)
+        return param_space_v
 
     def init_search(self):
         '''
