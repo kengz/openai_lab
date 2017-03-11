631c1a12ad57bdb45a1251a69ecd153f7fdb2e23
kengz <kengzwl@gmail.com>
2017-03-11 14:37:30 -0500

    absorb pass test into dev spec; override universally except it to speedup tests

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,7 @@ environ['CI'] = environ.get('CI') or 'true'
 
 def pytest_runtest_setup(item):
     for problem in rl.util.PROBLEMS:
-        if problem == 'CartPole-v0':
-            rl.util.PROBLEMS[problem]['MAX_EPISODES'] = 30
+        if problem == 'TestPassCartPole-v0':
+            pass
         else:
             rl.util.PROBLEMS[problem]['MAX_EPISODES'] = 3
