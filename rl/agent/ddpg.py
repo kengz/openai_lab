fd0a6d7f773cb3bad3b1d86e804feb53992557e7
kengz <kengzwl@gmail.com>
2017-03-12 13:49:23 -0400

    add random process and actor.select_action

--- rl/agent/ddpg.py
+++ rl/agent/ddpg.py
@@ -3,10 +3,42 @@ from rl.agent.base_agent import Agent
 from rl.util import logger, log_self, clone_model
 
 
+class RandomProcess(object):
+
+    def reset_states(self):
+        pass
+
+
+# Based on
+# http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
+class OrnsteinUhlenbeckProcess(AnnealedGaussianProcess):
+
+    def __init__(self, theta, mu=0., sigma=1., dt=1e-2, x0=None, size=1, sigma_min=None, n_steps_annealing=1000):
+        super(OrnsteinUhlenbeckProcess, self).__init__(
+            mu=mu, sigma=sigma, sigma_min=sigma_min, n_steps_annealing=n_steps_annealing)
+        self.theta = theta
+        self.mu = mu
+        self.dt = dt
+        self.x0 = x0
+        self.size = size
+        self.reset_states()
+
+    def sample(self):
+        x = self.x_prev + self.theta * \
+            (self.mu - self.x_prev) * self.dt + self.current_sigma * \
+            np.sqrt(self.dt) * np.random.normal(size=self.size)
+        self.x_prev = x
+        self.n_steps += 1
+        return x
+
+    def reset_states(self):
+        self.x_prev = self.x0 if self.x0 is not None else np.zeros(self.size)
+
+
 class ActorNetwork(object):
 
     '''
-    actor: mu(s|theta_mu), like our typical model(state)=action_Q_val
+    actor: mu(s|theta_mu), like our typical model(state)=action_val (cont)
     '''
 
     def __init__(self):
@@ -15,6 +47,8 @@ class ActorNetwork(object):
         from keras.models import Sequential
         self.Dense = Dense
         self.Sequential = Sequential
+        self.random_process = OrnsteinUhlenbeckProcess(
+            size=self.env_spec['action_dim'], theta=.15, mu=0., sigma=.3)
 
         # super(DQN, self).__init__(env_spec)
 
@@ -38,6 +72,18 @@ class ActorNetwork(object):
 
         self.actor = model
         self.target_actor = clone_model(self.actor)
+
+    def select_action(self, state):
+        action = self.actor.predict(state) + self.random_process.sample()
+        return action
+
+        # # Apply noise, if a random process is set.
+        # if self.training and self.random_process is not None:
+        #     noise = self.random_process.sample()
+        #     assert noise.shape == action.shape
+        #     action += noise
+
+        # return action
 
 
 class CriticNetwork(object):
