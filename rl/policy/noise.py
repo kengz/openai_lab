8a103d66d91ce46031b630dac2bd31a8ec960fcb
kengz <kengzwl@gmail.com>
2017-04-29 12:49:31 -0400

    remove old discrete encoding from ddpg policy

--- rl/policy/noise.py
+++ rl/policy/noise.py
@@ -24,15 +24,10 @@ class NoNoisePolicy(Policy):
     def select_action(self, state):
         agent = self.agent
         state = np.expand_dims(state, axis=0)
-        if self.env_spec['actions'] == 'continuous':
-            action = agent.actor.predict(state)[0] + self.sample()
-            action = np.clip(action,
-                             self.env_spec['action_bound_low'],
-                             self.env_spec['action_bound_high'])
-        else:
-            Q_state = agent.actor.predict(state)[0]
-            assert Q_state.ndim == 1
-            action = np.argmax(Q_state)
+        action = agent.actor.predict(state)[0] + self.sample()
+        action = np.clip(action,
+                         self.env_spec['action_bound_low'],
+                         self.env_spec['action_bound_high'])
         return action
 
     def update(self, sys_vars):
